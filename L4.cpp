bd8cab9d32d352051c6cc7b73d31901125f3e32c
TomerEiny8 <tomerkorial@gmail.com>
2025-06-21 12:05:44 +0300

    L4 with functions

--- L4.cpp
+++ L4.cpp
@@ -51,14 +51,92 @@ l4_packet::l4_packet(const std::string& raw_data) {
 	unsigned int l4_packet::get_addrs() const { return this->addrs; }
 
 
+	/*
+	 * @fn validate_packet
+	 * @brief Check whether the packet is valid.
+     * @param [in] open_ports - Vector containing all the NIC's open ports.
+     * @param [in] ip - NIC's IP address.
+     * @param [in] mask - NIC's mask; together with the IP,
+     *             determines the NIC's local net.
+     * @param [in] mac - NIC's MAC address.
+     *
+     * @return true if the packet is valid and ready for processing.
+     *         false if the packet isn't valid and should be discarded.
+     */
 	bool l4_packet::validate_packet(open_port_vec open_ports,
-            uint8_t ip[IP_V4_SIZE],
-            uint8_t mask,
-            uint8_t mac[MAC_SIZE]) {return true;}
+	                                uint8_t ip[IP_V4_SIZE],
+	                                uint8_t mask,
+	                                uint8_t mac[MAC_SIZE]) {
+		bool is_src_pckt = false;
+		bool is_dst_pckt = false;
+		if(this->src_prt == this->dst_prt) {
+			return false;
+		}
+		if(this->addrs < 0 || this->addrs >= 32){
+			return false;
+		}
+		for(open_port& port : open_ports){
+			if(this->dst_prt == port.dst_prt) {
+				is_dst_pckt = true;
+			}
+		}
+		for(open_port& port : open_ports){
+			if(this->src_prt == port.src_prt) {
+				is_src_pckt = true;
+			}
+		}
+		if(is_src_pckt && is_dst_pckt){
+			return true;
+		}
+		return false;
+	}
 
-	bool l4_packet::proccess_packet(open_port_vec &open_ports,
-								uint8_t ip[IP_V4_SIZE],
-								uint8_t mask,
-								memory_dest &dst) {return true;}
+	/**
+     * @fn proccess_packet
+     * @brief Modify the packet and return the memory location it should be
+     *        stored in. In the case of local DRAM, the function will store
+	 *        the packet as a string in the relevant 'open_port' struct.
+     *
+     * @param [in] open_ports - Vector containing all the NIC's open ports.
+     * @param [in] ip - NIC's IP address.
+     * @param [in] mask - NIC's mask; together with the IP, determines the NIC's
+     *        local net.
+     * @param [out] dst - Reference to enum that indicate the memory space where
+     *        the packet should be stored:
+     *         LOCAL_DRAM - the function stored it to the currect struct.
+     *         RQ - Should be stored as a string in RQ.
+     *         TQ - Should be stored as string in TQ.
+     *
+     * @return true in success, false in failure (e.g memory allocation failed).
+     */
 
-	bool l4_packet::as_string(std::string &packet) {return true;}
+    bool l4_packet::proccess_packet(open_port_vec &open_ports,
+                            		uint8_t ip[IP_V4_SIZE],
+									uint8_t mask,
+									memory_dest &dst) {
+    	if(!this->validate_packet(open_ports, ip, mask, NULL){
+    		return false;
+    	}
+    	for(open_port& port : open_ports){
+    		if(this->dst_prt == port.dst_prt) {
+    			port.data[this->addrs] = this->data;
+    		}
+    	}
+    	memory_dest = LOCAL_DRAM; // Local DRAM
+    	return true;
+    }
+
+    /**
+     * @fn as_string
+     * @brief Convert the packet to string.
+     *
+     * @param [out] packet - Packet as a string, ready to be stored in RQ/TQ.
+     *
+     * @return true in success, false in failure (e.g memory allocation failed).
+     */
+    bool l4_packet::as_string(std::string &packet){
+    	packet = std::to_string(this->src_prt) + "|" +
+    			 std::to_string(this->dst_prt) + "|" +
+				 this->data;
+    	return true;
+    }
